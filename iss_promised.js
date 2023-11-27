3c9976103537a9806af3332be176dd64aa72cadb
gisellealmario <gisellealmario@gmail.com>
2023-11-27 01:01:45 +0000

    promised iss

---
+++ iss_promised.js
@@ -0,0 +1,28 @@
+const request = require('request-promise-native');
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}`);
+};
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+//Instead of exporting each of these 3 functions from iss_promised.js, let's simply export a function nextISSTimesForMyLocation which chains these function calls for us and returns the final results.
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
